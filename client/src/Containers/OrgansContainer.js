f1dea78c7c2f1b16ab81724dfdf357531a1127b8
sreadytocode <sara.younis2303@gmail.com>
2022-11-11 22:55:07 +0000

    Connected heading to container

--- client/src/Containers/OrgansContainer.js
+++ client/src/Containers/OrgansContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Heading from "../Components/Heading.js"
 
 const OrgansContainer = () => {
     return ( 
         <>
+        <Heading text="Educational Organ Webpage"/>
         </>
      );
 }
