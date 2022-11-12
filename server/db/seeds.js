a939a99eb72de99950e9ca7401bf6b98453d9dc0
sreadytocode <sara.younis2303@gmail.com>
2022-11-12 00:35:56 +0000

    Fixed syntax errors with db

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -9,11 +9,11 @@ db.bodyparts.insertMany(
                 name: "Brain",
                 diagram: "https://bam.files.bbci.co.uk/bam/live/content/z38cmsg/large",
                 facts: {
-                    "The brain controls complex behaviour. It is made of billions of connected neurones and has different regions that carry out different functions.",
-                    "The Cerebrum is one of the four main areas of the brain. It has an outer layer called cerebral cortex which is split into two sections. This part controls intelligience, personality, concious thought, language and verbal memory.",
-                    "Cerebellum controls balance, co-ordination of movement and muscular activity.",
-                    "Medulla controls unconscious activities such as heart rate and breathing rate",
-                    "Hypothalamus is the regulating centre for temperature and water balance for the body",
+                    0: "The brain controls complex behaviour. It is made of billions of connected neurones and has different regions that carry out different functions.",
+                    1: "The Cerebrum is one of the four main areas of the brain. It has an outer layer called cerebral cortex which is split into two sections. This part controls intelligience, personality, concious thought, language and verbal memory.",
+                    2: "Cerebellum controls balance, co-ordination of movement and muscular activity.",
+                    3: "Medulla controls unconscious activities such as heart rate and breathing rate",
+                    4: "Hypothalamus is the regulating centre for temperature and water balance for the body"
                 },
                 quiz: {
                     quiz_question_one: "Is the brain made up of 4 main areas?",
@@ -33,11 +33,11 @@ db.bodyparts.insertMany(
            name: "lungs",
            diagram: "https://ichef.bbci.co.uk/images/ic/448xn/p08vwby2.png",
            facts: {
-               fact: "When you breathe in, air travels down into your alveoli.",
-               fact: "Lungs release waste gases back into the environment - called expiration.",
-               fact: "Carbon dioxide is produced as a waste product of respiration",
-               fact: "It travels all around your body in your bloodstream and is delivered to every cell where it is used for respiration to provide you with energy.",
-               fact: "An average person breathes in around 11,000 liters of air every day."
+                0: "When you breathe in, air travels down into your alveoli.",
+                1: "Lungs release waste gases back into the environment - called expiration.",
+                2: "Carbon dioxide is produced as a waste product of respiration",
+                3: "It travels all around your body in your bloodstream and is delivered to every cell where it is used for respiration to provide you with energy.",
+                4: "An average person breathes in around 11,000 liters of air every day."
            },
            
            quiz: {
