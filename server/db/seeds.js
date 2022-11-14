a65f5850692d5c4541b517675f2c2e57efe77fa7
Takako Onozuka <onozukatakako@gmail.com>
2022-11-14 13:27:59 +0000

    changed seeds"

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,70 +6,92 @@ db.bodyparts.insertMany(
             {
                 name: "Brain",
                 diagram: "https://bam.files.bbci.co.uk/bam/live/content/z38cmsg/large",
-                facts: {
-                    0: "The brain controls complex behaviour. It is made of billions of connected neurones and has different regions that carry out different functions.",
-                    1: "The Cerebrum is one of the four main areas of the brain. It has an outer layer called cerebral cortex which is split into two sections. This part controls intelligience, personality, concious thought, language and verbal memory.",
-                    2: "Cerebellum controls balance, co-ordination of movement and muscular activity.",
-                    3: "Medulla controls unconscious activities such as heart rate and breathing rate",
-                    4: "Hypothalamus is the regulating centre for temperature and water balance for the body"
-                },
-                quiz: {
-                    quiz_question_one: "Is the brain made up of 4 main areas?",
-                    quiz_answer_one: true,
-                    quiz_question_two: "Does the cerebrum control the bodies temperature?",
-                    quiz_answer_two: false,
-                    quiz_question_three: "Does the Cerebellum control balance and muscular activity?",
-                    quiz_answer_three: true,
-                    quiz_question_four: "Is the hypothalamus the regulating centre for the body?",
-                    quiz_answer_four: true,
-                }
+                facts: [
+                    "The brain controls complex behaviour. It is made of billions of connected neurones and has different regions that carry out different functions.",
+                    "The Cerebrum is one of the four main areas of the brain. It has an outer layer called cerebral cortex which is split into two sections. This part controls intelligience, personality, concious thought, language and verbal memory.",
+                    "Cerebellum controls balance, co-ordination of movement and muscular activity.",
+                    "Medulla controls unconscious activities such as heart rate and breathing rate",
+                    "Hypothalamus is the regulating centre for temperature and water balance for the body"
+                ],
+                quiz: [
+                    {
+                    question: "Is the brain made up of 4 main areas?",
+                    answer: true
+                    },
+                    {
+                    question: "Does the cerebrum control the bodies temperature?",
+                    answer: false
+                    },
+                    {
+                    question: "Does the Cerebellum control balance and muscular activity?",
+                    answer: true
+                    },
+                    {
+                    question: "Is the hypothalamus the regulating centre for the body?",
+                    answer: true
+                    }
+                ]
             },
         {
            name: "lungs",
            diagram: "https://ichef.bbci.co.uk/images/ic/448xn/p08vwby2.png",
-           facts: {
-                0: "When you breathe in, air travels down into your alveoli.",
-                1: "Lungs release waste gases back into the environment - called expiration.",
-                2: "Carbon dioxide is produced as a waste product of respiration",
-                3: "It travels all around your body in your bloodstream and is delivered to every cell where it is used for respiration to provide you with energy.",
-                4: "An average person breathes in around 11,000 liters of air every day."
-           },
-                quiz: {
-                    quiz_question_one: "The air you inhale travels down into alveoli.",
-                    quiz_answer: true,
-                    quiz_question_two: "An average person breathes in around 100 liters of air per day.",
-                    quiz_answer_two: false,
-                    quiz_question_three: "Expiration is the act of breathing in oxigen.",
-                    quiz_answer_three: false,
-                    quiz_question_four: "Carbon dioxide is produced as a result of respiration.",
-                    quiz_answer_four: true
-                }
-        },
+           facts: [
+                "When you breathe in, air travels down into your alveoli.",
+                "Lungs release waste gases back into the environment - called expiration.",
+                "Carbon dioxide is produced as a waste product of respiration",
+                "It travels all around your body in your bloodstream and is delivered to every cell where it is used for respiration to provide you with energy.",
+                "An average person breathes in around 11,000 liters of air every day."
+           ],
+                quiz: [
+                    {
+                    question: "The air you inhale travels down into alveoli.",
+                    answer: true
+                    },
+                    {
+                    question: "An average person breathes in around 100 liters of air per day.",
+                    answer: false
+                    }, 
+                    {
+                    question: "Expiration is the act of breathing in oxigen.",
+                    answer: false
+                    },
+                    {
+                    question: "Carbon dioxide is produced as a result of respiration.",
+                    answer: true
+                    }
+                ]
+         },
+
         {
 
             name: "Heart",
             diagram: 'https://pmgbiology.files.wordpress.com/2015/02/heart.png',
-            facts: {
-                0: "A fist-sized organ that pumps blood throughout the body",
-                1: "The primary organ of the circulatory system",
-                1: "Contains four main sections, called chambers, made of muscles",
-                2: "The muscles are powered by electrical impulses",
-                3: "The brain and nervous system direct the heart's function"
-            },
-
-
-            quiz: {
-                quiz_question_one: "The heart has three chambers.",
-                quiz_answer_one: false,
-                quiz_question_two: "The heart is responsible for pumping blood throughout the body.",
-                quiz_answer_two: true,
-                quiz_question_three: "The heart is about the size of a fist.",
-                quiz_answer_three: true,
-                quiz_question_four: "The nervous system directs the heart's function.",
-                quiz_answer_four: true,
-            }
-
-
+            facts: [
+                "A fist-sized organ that pumps blood throughout the body",
+                "The primary organ of the circulatory system",
+                "Contains four main sections, called chambers, made of muscles",
+                "The muscles are powered by electrical impulses",
+                "The brain and nervous system direct the heart's function"
+            ],
+            
+            quiz: [
+                {
+                question: "The heart has three chambers.",
+                answer: false
+                },
+                {
+                question: "The heart is responsible for pumping blood throughout the body.",
+                answer: true
+                },
+                {
+                question: "The heart is about the size of a fist.",
+                answer: true,
+                },
+                {
+                question: "The nervous system directs the heart's function.",
+                answer: true
+                }
+            ]
         }
 
     ])
