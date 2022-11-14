1940a9ef1501933f02ac9c2a0e95758f3743bf7d
Dahlia Peterson <dahlia.valle@gmail.com>
2022-11-14 17:07:32 +0000

    Updated Bullet Points

--- client/src/Components/OrganSelect.js
+++ client/src/Components/OrganSelect.js
@@ -1,26 +1,29 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getOrgans, organById } from "./OrgansService.js";
+import BulletPoints from "./Re-usable/BulletPoints.js";
 
 const OrganSelect = () => {
-    const [organ, setOrgan] = useState([]);
+    const [organ, setOrgan] = useState({});
     const { id } = useParams()
-    
 
-     useEffect (() => {
+
+    useEffect(() => {
         organById(id)
-        .then((info) => {
-            setOrgan(info) 
-        }
-       )
-    },[id]);
+            .then((info) => {
+                console.log(info)
+                setOrgan(info)
+            }
+            )
+    }, [id]);
 
-    return ( 
+    return (
         <div>
             <h2>{organ.name}</h2>
-            <img src={organ.diagram} alt="Organ-diagram" width="350"/>
+            <img src={organ.diagram} alt="Organ-diagram" width="350" />
+            <BulletPoints facts={organ.facts} />
         </div>
-     );
+    );
 }
- 
+
 export default OrganSelect;
