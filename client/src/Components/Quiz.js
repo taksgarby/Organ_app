e3690175fd590fe90303cf89c724fccb2a7fcc13
sreadytocode <sara.younis2303@gmail.com>
2022-11-14 17:50:53 +0000

    Added UseEffect and checked via console.log to ensure retrieving data. Did a h1 with name in jsx to make sure am getting the name. Removed console.log

--- client/src/Components/Quiz.js
+++ client/src/Components/Quiz.js
@@ -2,14 +2,14 @@ import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import { getOrgans, organById } from "./OrgansService.js";
 
-const Quiz = ({organs}) => {
+const Quiz = () => {
     const [questions, setQuestions] = useState([]);
     const { id } = useParams()
 
     useEffect (() => {
         organById(id)
         .then((info) => {
-            console.log(setQuestions(info)) 
+            setQuestions(info) 
         }
        )
     },[id]);
