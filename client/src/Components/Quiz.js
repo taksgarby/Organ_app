5a06b36473491ee24783f9969ad7c99b40d4156e
sreadytocode <sara.younis2303@gmail.com>
2022-11-15 00:19:01 +0000

    Added showscore state

--- client/src/Components/Quiz.js
+++ client/src/Components/Quiz.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import { organById } from "./OrgansService.js";
-import Button from './Re-usable/Button.js';
 import Questions from "./Re-usable/Questions.js";
 import Answers from "./Re-usable/Answers.js";
 
 const Quiz = () => {
     const [quiz, setQuiz] = useState([]);
+    const [score, setScore] = useState(); 
+
     const { id } = useParams()
 
     useEffect (() => {
@@ -22,11 +23,11 @@ const Quiz = () => {
     const questionList = quiz.map((the, index) => {
         return (
             <div key={index}>
+                    <form>
                 <div>
                     <Questions the={the}/>
 
                 </div>
-                    <form>
                 <div>
                     <Answers the={the}/>
                  </div> 
@@ -36,9 +37,9 @@ const Quiz = () => {
     })
 
     return ( 
-        <>
-            {questionList}
-        </>
+        <div>
+                {questionList}
+        </div>
      );
 }
  
