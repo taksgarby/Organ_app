a265a5009596f14165b18ccc289b12117398c51f
Dahlia Peterson <dahlia.valle@gmail.com>
2022-11-16 16:45:27 +0000

    Box around bulletpoints

--- client/src/Components/AboutUs.js
+++ client/src/Components/AboutUs.js
@@ -37,55 +37,55 @@ justify-content:space-around;
 font-size: 1.3em;
 `
 const ContributerName = styled.p`
-    color: ${(props)=>props.color}
+    color: ${(props) => props.color}
 
 `
 
-const AboutUs = ({contributers}) => {
+const AboutUs = ({ contributers }) => {
 
 
     const eachPerson = contributers.map(contributer => {
         return (
-            <div key = {contributer._id}>
+            <div key={contributer._id}>
                 <ContributerName color="#27296d"><strong>{contributer.name}</strong></ContributerName>
                 <p>Hometown:</p>
-                    <p> {contributer.Hometown}</p>
+                <p> {contributer.Hometown}</p>
                 <p>Favourite Food:</p>
                 <p>{contributer.FavouriteFood}</p>
                 <p>Find more about: </p>
                 <p><a href={contributer.MoreInfo}>{contributer.name}</a></p>
-     
+
             </div>
         )
     })
 
 
-  
 
 
-    return ( 
+
+    return (
         <>
-        <Heading text={"About Us"}/>
-        <HeaderTwo>About this app</HeaderTwo>
-        <ParaText>
-        We have created an app for BBC Bitesize that will teach children about organs. Our target audience is children aged 14-16. In addition to providing key information about each organ, we have created an interactive quiz they can use to test their knowledge. Each organ comes with its own page that has a diagram  and a bullet point list of key take aways about the organ. 
-        </ParaText>
-        <HeaderTwo>Contributers</HeaderTwo>
-        <SubHeader>We are a team of hardworking individuals from diverse backgrounds</SubHeader>
-        <FlexContainer>
-        
+            <Heading text={"About Us"} />
+            <HeaderTwo>About this app</HeaderTwo>
+            <ParaText>
+                We have created an app for BBC Bitesize that will teach children about organs. Our target audience is children aged 14-16. In addition to providing key information about each organ, we have created an interactive quiz they can use to test their knowledge. Each organ comes with its own page that has a diagram  and a bullet point list of key take aways about the organ.
+            </ParaText>
+            <HeaderTwo>Contributers</HeaderTwo>
+            <SubHeader>We are a team of hardworking individuals from diverse backgrounds</SubHeader>
+            <FlexContainer>
 
-            {eachPerson}
 
-       
-        
+                {eachPerson}
 
-        </FlexContainer>
+
+
+
+            </FlexContainer>
 
         </>
-     );
+    );
 }
- 
+
 export default AboutUs;
 
 
--- client/src/Components/Re-usable/BulletPoints.js
+++ client/src/Components/Re-usable/BulletPoints.js
@@ -9,15 +9,23 @@ const Li = styled.li`
 margin: 1.2em;
 `
 
+const FlexContainer = styled.div`
+box-sizing:border-box;
+width:100%;
+margin: 1em;
+border-style: outset;`
+
 const BulletPoints = ({ facts }) => {
     if (!facts) { return null }
     const lielements = facts.map((fact, index) => {
         return (<Li key={index}>{fact} </Li>)
     })
     return (
-        <Ul>
-            {lielements}
-        </Ul>
+        <FlexContainer>
+            <Ul>
+                {lielements}
+            </Ul>
+        </FlexContainer>
     )
 }
 export default BulletPoints; 
